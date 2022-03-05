55905730c7aba2043d5890418e4d6da16f18bff9
Wandir Pereira Filho <wandir@gmail.com>
2022-03-05 07:51:03 -0300

    Reestruturando o projeto

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using ProEventos.API.Data;
-using ProEventos.API.Models;
+using ProEventos.Domain;
+using ProEventos.Persistence.Contextos;
 
 namespace ProEventos.API.Controllers
 {
@@ -10,28 +10,8 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventosController : ControllerBase
     {
-        // public IEnumerable<Evento> _evento = new Evento[] {
-        //         new Evento() {
-        //             EventoId = 1,
-        //             Tema = "Angular 11 e Net. 5",
-        //             Local = "BElo Horizonte",
-        //             Lote = "1º Lote",
-        //             QtdPessoas = 250,
-        //             DataEvento =  DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-        //             ImagemURL = "foto.png"
-        //         },
-        //         new Evento() {
-        //             EventoId = 2,
-        //             Tema = "Angular 11 e Net. 5",
-        //             Local = "BElo Horizonte",
-        //             Lote = "1º Lote",
-        //             QtdPessoas = 250,
-        //             DataEvento =  DateTime.Now.AddDays(2).ToString(),
-        //             ImagemURL = "foto2.png"
-        //         }
-        //     };
-        private readonly DataContext _context;
-        public EventosController(DataContext context)
+        private readonly ProEventosContext _context;
+        public EventosController(ProEventosContext context)
         {
             this._context = context;
         }
@@ -45,7 +25,7 @@ namespace ProEventos.API.Controllers
         [HttpGet("{id}")]
         public Evento GetByID(int id)
         {
-            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.Id == id);
         }
     }
 }
